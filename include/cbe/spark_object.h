7f05d767f48e0727e5503f2f08cf3efaaeef5639
Josef Söntgen <josef.soentgen@genode-labs.com>
2019-07-18 14:37:26 +0200

    cbe: fix license header in include directory

--- include/cbe/spark_object.h
+++ include/cbe/spark_object.h
@@ -1,14 +1,9 @@
 /*
- * \brief  CBE SPARK object allocator helper
- * \author Josef Soentgen
- * \date   2019-05-20
- */
-
-/*
- * Copyright (C) 2019 Genode Labs GmbH
+ * Copyright (C) 2019 Genode Labs GmbH, Componolit GmbH, secunet AG
  *
- * This file is part of the Genode OS framework, which is distributed
- * under the terms of the GNU Affero General Public License version 3.
+ * This file is part of the Consistent Block Encrypter project, which is
+ * distributed under the terms of the GNU Affero General Public License
+ * version 3.
  */
 
 #ifndef _CBE_SPARK_OBJECT_H_
