4388c25486ef9a659d1ce8c568fbc1bbb21ec8be
Josef Söntgen <josef.soentgen@genode-labs.com>
2019-08-12 17:36:36 +0200

    lib/cbe: add missing Sync_superblock files

---
+++ src/lib/cbe/cbe-sync_superblock.adb
@@ -0,0 +1,223 @@
+--
+-- Copyright (C) 2019 Genode Labs GmbH, Componolit GmbH, secunet AG
+--
+-- This file is part of the Consistent Block Encrypter project, which is
+-- distributed under the terms of the GNU Affero General Public License
+-- version 3.
+--
+
+pragma Ada_2012;
+
+with CBE.Request;
+
+package body CBE.Sync_superblock
+with Spark_Mode
+is
+
+	use type Request.Block_Number_Type;
+
+	package body Item
+	with Spark_Mode
+	is
+		--
+		-- Pending_Item
+		--
+		procedure Invalid_Item(
+			Obj  : out Item_Type)
+		is
+		begin
+			Set_State(Obj, Sta => Invalid);
+
+			Obj.Idx := 0;
+			Obj.Gen := 0;
+		end Invalid_Item;
+
+		--
+		-- Pending_Item
+		--
+		procedure Pending_Item(
+			Obj : out Item_Type;
+			Idx :     Superblock_Index_Type;
+			Gen :     Generation_Type)
+		is
+		begin
+			Set_State(Obj, Sta => Pending);
+
+			Obj.Idx := Idx;
+			Obj.Gen := Gen;
+		end Pending_Item;
+
+		---------------
+		-- Accessors --
+		---------------
+
+		function Invalid     (Obj : Item_Type) return Boolean is (Obj.Sta = Invalid);
+		function Pending     (Obj : Item_Type) return Boolean is (Obj.Sta = Pending);
+		function In_Progress (Obj : Item_Type) return Boolean is (Obj.Sta = In_Progress);
+		function Complete    (Obj : Item_Type) return Boolean is (Obj.Sta = Complete);
+
+		function State      (Obj : Item_Type) return State_Type is (Obj.Sta);
+		function Index      (Obj : Item_Type) return Superblock_Index_Type is (Obj.Idx);
+		function Generation (Obj : Item_Type) return Generation_Type is (Obj.Gen);
+
+		procedure Set_State(
+			Obj : in out Item_Type;
+			Sta :        State_Type)
+		is
+		begin
+			Obj.Sta := Sta;
+		end Set_State;
+	end Item;
+
+	--
+	-- Initialize_Object
+	--
+	procedure Initialize_Object(Obj : out Object_Type)
+	is
+	begin
+		Item.Invalid_Item(Obj.Current_Item);
+		Obj.Current_Primitive := Primitive.Invalid_Object;
+	end Initialize_Object;
+
+	--
+	-- Request_Acceptable
+	--
+	function Request_Acceptable(Obj : Object_Type)
+	return Boolean
+	is
+	begin
+		return not Primitive.Valid(Obj.Current_Primitive);
+	end Request_Acceptable;
+
+	--
+	-- Submit_Request
+	--
+	procedure Submit_Request(
+		Obj : in out Object_Type;
+		Idx :        Superblock_Index_Type;
+		Gen :        Generation_Type)
+	is
+	begin
+		Item.Pending_Item(Obj.Current_Item, Idx, Gen);
+		Obj.Current_Primitive := Primitive.Valid_Object(
+			Op     => Request.Write,
+			Succ   => Request.Success_Type(False),
+			Tg     => 16#80#,
+			-- there is currently a 1:1 mapping between SB slot and pba
+			Blk_Nr => Request.Block_Number_Type(Item.Index(Obj.Current_Item)),
+			Idx    => 0);
+	end Submit_Request;
+
+	--
+	-- Peek_Completed_Primitive
+	--
+	function Peek_Completed_Primitive(Obj : Object_Type)
+	return Primitive.Object_Type
+	is
+	begin
+		if Item.Complete(Obj.Current_Item)
+		then
+			return Obj.Current_Primitive;
+		end if;
+		return Primitive.Invalid_Object;
+	end Peek_Completed_Primitive;
+
+	--
+	-- Peek_Completed_Generation
+	--
+	function Peek_Completed_Generation(
+		Obj  : Object_Type;
+		Prim : Primitive.Object_Type)
+	return Generation_Type
+	is
+	begin
+		if Item.Complete(Obj.Current_Item) and
+		   Primitive.Block_Number(Obj.Current_Primitive) = Primitive.Block_Number(Prim)
+		then
+			return Item.Generation(Obj.Current_Item);
+		end if;
+		return Generation_Type(0);
+	end Peek_Completed_Generation;
+
+	--
+	-- Drop_Completed_Primitive
+	--
+	procedure Drop_Completed_Primitive(
+		Obj  : in out Object_Type;
+		Prim :        Primitive.Object_Type)
+	is
+	begin
+		if Item.Complete(Obj.Current_Item) and
+		   Primitive.Block_Number(Obj.Current_Primitive) = Primitive.Block_Number(Prim)
+		then
+			Item.Invalid_Item(Obj.Current_Item);
+			Obj.Current_Primitive := Primitive.Invalid_Object;
+			return;
+		end if;
+	end Drop_Completed_Primitive;
+
+	--
+	-- Peek_Generated_Primitive
+	--
+	function Peek_Generated_Primitive(Obj : Object_Type)
+	return Primitive.Object_Type
+	is
+	begin
+		if Item.Pending(Obj.Current_Item)
+		then
+			return Obj.Current_Primitive;
+		end if;
+
+		return Primitive.Invalid_Object;
+	end Peek_Generated_Primitive;
+
+	--
+	-- Peek_Generated_Index
+	--
+	function Peek_Generated_Index(
+		Obj  : Object_Type;
+		Prim : Primitive.Object_Type)
+	return Superblock_Index_Type
+	is
+	begin
+		if Item.Pending(Obj.Current_Item) and
+		   Primitive.Block_Number(Obj.Current_Primitive) = Primitive.Block_Number(Prim)
+		then
+			return Item.Index(Obj.Current_Item);
+		end if;
+		return Superblock_Index_Type(0);
+	end Peek_Generated_Index;
+
+	--
+	-- Drop_Generated_Primitive
+	--
+	procedure Drop_Generated_Primitive(
+		Obj  : in out Object_Type;
+		Prim :        Primitive.Object_Type)
+	is
+	begin
+		if Item.Pending(Obj.Current_Item) and
+		   Primitive.Block_Number(Obj.Current_Primitive) = Primitive.Block_Number(Prim)
+		then
+			Item.Set_State(Obj.Current_Item, Item.In_Progress);
+			return;
+		end if;
+	end Drop_Generated_Primitive;
+
+	--
+	-- Mark_Generated_Primitive_Complete
+	--
+	procedure Mark_Generated_Primitive_Complete(
+		Obj  : in out Object_Type;
+		Prim :        Primitive.Object_Type)
+	is
+	begin
+		if Item.In_Progress(Obj.Current_Item) and
+		   Primitive.Block_Number(Obj.Current_Primitive) = Primitive.Block_Number(Prim)
+		then
+			Item.Set_State(Obj.Current_Item, Item.Complete);
+			Primitive.Success(Obj.Current_Primitive, Primitive.Success(Prim));
+			return;
+		end if;
+	end Mark_Generated_Primitive_Complete;
+end CBE.Sync_superblock;
---
+++ src/lib/cbe/cbe-sync_superblock.ads
@@ -0,0 +1,157 @@
+--
+-- Copyright (C) 2019 Genode Labs GmbH, Componolit GmbH, secunet AG
+--
+-- This file is part of the Consistent Block Encrypter project, which is
+-- distributed under the terms of the GNU Affero General Public License
+-- version 3.
+--
+
+pragma Ada_2012;
+
+with CBE.Primitive;
+
+package CBE.Sync_Superblock
+with Spark_Mode
+is
+	pragma Pure;
+
+	type Object_Type is private;
+
+	--
+	-- Initialize_Object
+	--
+	procedure Initialize_Object(Obj : out Object_Type);
+
+	--
+	-- Request_Acceptable
+	--
+	function Request_Acceptable(Obj : Object_Type)
+	return Boolean;
+
+	--
+	-- Submit_Request
+	--
+	procedure Submit_Request(
+		Obj : in out Object_Type;
+		Idx :        Superblock_Index_Type;
+		Gen :        Generation_Type);
+
+	--
+	-- Peek_Completed_Primitive
+	--
+	function Peek_Completed_Primitive(Obj : Object_Type)
+	return Primitive.Object_Type;
+
+	--
+	-- Peek_Completed_Generation
+	--
+	function Peek_Completed_Generation(
+		Obj  : Object_Type;
+		Prim : Primitive.Object_Type)
+	return Generation_Type;
+
+
+	--
+	-- Drop_Completed_Primitive
+	--
+	procedure Drop_Completed_Primitive(
+		Obj  : in out Object_Type;
+		Prim :        Primitive.Object_Type)
+	with
+		Pre => Primitive.Valid(Prim);
+
+	--
+	-- Peek_Generated_Primitive
+	--
+	function Peek_Generated_Primitive(Obj : Object_Type)
+	return Primitive.Object_Type;
+
+	--
+	-- Peek_Generated_Index
+	--
+	function Peek_Generated_Index(
+		Obj  : Object_Type;
+		Prim : Primitive.Object_Type)
+	return Superblock_Index_Type
+	with
+		Pre => Primitive.Valid(Prim);
+
+	--
+	-- Drop_Generated_Primitive
+	--
+	procedure Drop_Generated_Primitive(
+		Obj  : in out Object_Type;
+		Prim :        Primitive.Object_Type)
+	with
+		Pre => Primitive.Valid(Prim);
+
+	--
+	-- Mark_Generated_Primitive_Complete
+	--
+	procedure Mark_Generated_Primitive_Complete(
+		Obj  : in out Object_Type;
+		Prim :        Primitive.Object_Type);
+
+private
+
+	--
+	-- Item
+	--
+	package Item
+	with Spark_Mode
+	is
+		type State_Type is (Invalid, Pending, In_Progress, Complete);
+		type Item_Type  is private;
+
+		--
+		-- Invalid_Item
+		--
+		procedure Invalid_Item(Obj  : out Item_Type);
+
+		--
+		-- Pending_Item
+		--
+		procedure Pending_Item(
+			Obj : out Item_Type;
+			Idx :     Superblock_Index_Type;
+			Gen :     Generation_Type);
+
+		---------------
+		-- Accessors --
+		---------------
+
+		function Invalid     (Obj : Item_Type) return Boolean;
+		function Pending     (Obj : Item_Type) return Boolean;
+		function In_Progress (Obj : Item_Type) return Boolean;
+		function Complete    (Obj : Item_Type) return Boolean;
+
+		function State      (Obj : Item_Type) return State_Type;
+		function Index      (Obj : Item_Type) return Superblock_Index_Type;
+		function Generation (Obj : Item_Type) return Generation_Type;
+
+		procedure Set_State(
+			Obj : in out Item_Type;
+			Sta :        State_Type);
+
+	private
+
+		--
+		-- Item_Type
+		--
+		type Item_Type is record
+			Idx : Superblock_Index_Type;
+			Gen : Generation_Type;
+			Sta : State_Type;
+		end record;
+
+	end Item;
+
+	--
+	-- Object_Type
+	--
+	type Object_Type is record
+		Current_Primitive : Primitive.Object_Type;
+		Current_Item      : Item.Item_Type;
+	end record;
+
+end CBE.Sync_Superblock;
