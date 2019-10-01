0fecb2b4321e690403a37d4619dc28cb967221b5
Johannes Kliemann <kliemann@componolit.com>
2019-10-01 16:57:07 +0200

    add cbe server write

--- src/gneiss/server/cbe/conversion.ads
+++ src/gneiss/server/cbe/conversion.ads
@@ -15,4 +15,10 @@ is
       with procedure Collect (V2 : T2);
    procedure Pass_In (V1 : T1);
 
+   generic
+      type T1 is private;
+      type T2 is private;
+      with procedure Collect (V2 : out T2);
+   procedure Pass_Out (V1 : out T1);
+
 end Conversion;
