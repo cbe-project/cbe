476f5889d1b6a065ba5dfc7acc9a600bb2a6e99e
Alexander Senier <senier@componolit.com>
2019-10-14 22:54:30 +0200

    Add memlimit

--- .ci/prove.mk
+++ .ci/prove.mk
@@ -2,4 +2,4 @@ OUTDIR=$(shell pwd)/out
 
 prove_spark_ci:
 	make -C .ci/lsc NO_SPARK=1 DESTDIR=$(OUTDIR)/lsc install
-	gnatprove -j0 --prover=z3,cvc4 --steps=6000 --checks-as-errors --warnings=error -U -P .ci/cbe.gpr
+	gnatprove -j0 --prover=z3,cvc4 --steps=1000 --memlimit=1000 --checks-as-errors --warnings=error -U -P .ci/cbe.gpr
