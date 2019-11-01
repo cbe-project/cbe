531414610b5b0d8ffe329e302d7b9745aaee31ad
Martin Stein <martin.stein@genode-labs.com>
2019-11-01 14:45:44 +0100

    .ci/prove.mk: set mode via env variable PROVE_MODE
    Default mode remains 'all'.

--- .ci/prove.mk
+++ .ci/prove.mk
@@ -1,5 +1,7 @@
 OUTDIR=$(shell pwd)/out
 
+PROVE_MODE ?= all
+
 prove_spark_ci:
 	make -C .ci/lsc NO_SPARK=1 DESTDIR=$(OUTDIR)/lsc install
-	gnatprove -j0 --prover=z3,cvc4 --steps=1000 --memlimit=1000 --checks-as-errors --warnings=error -U -P .ci/cbe.gpr
+	gnatprove --mode=$(PROVE_MODE) -j0 --prover=z3,cvc4 --steps=1000 --memlimit=1000 --checks-as-errors --warnings=error -U -P .ci/cbe.gpr
