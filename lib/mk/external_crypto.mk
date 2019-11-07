f44fdadfa3a68b2d90f857559b5cd29846e26721
Johannes Kliemann <kliemann@componolit.com>
2019-11-07 17:30:34 +0100

    add external_crypto library import

--- lib/mk/external_crypto.mk
+++ lib/mk/external_crypto.mk
@@ -3,6 +3,7 @@ LIBS    += cbe spark aes_cbc_4k
 INC_DIR += $(REP_DIR)/src/lib/cbe
 INC_DIR += $(REP_DIR)/src/lib/external_crypto
 
+SRC_ADS += external.ads
 SRC_ADB += external-crypto.adb
 
 vpath % $(REP_DIR)/src/lib/external_crypto
--- src/gneiss/server/cbe/target.mk
+++ src/gneiss/server/cbe/target.mk
@@ -1,4 +1,4 @@
 TARGET = cbe
 SRC_ADB += component.adb conversion.adb cbe_debug.adb
-LIBS += base spark gneiss libsparkcrypto cbe aes_cbc_4k sha256_4k
+LIBS += base spark gneiss libsparkcrypto cbe aes_cbc_4k sha256_4k external_crypto
 INC_DIR += $(PRG_DIR)
