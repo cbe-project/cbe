89eb0e64ca258aa274397d054898eee0134c3189
Josef Söntgen <josef.soentgen@genode-labs.com>
2019-11-11 17:18:11 +0100

    lib/cbe: fix active snapshot list binding

--- src/lib/cbe_cxx/cbe-cxx-cxx_library.ads
+++ src/lib/cbe_cxx/cbe-cxx-cxx_library.ads
@@ -71,8 +71,7 @@ is
       Export,
       Convention    => C,
       External_Name =>
-         "_ZN3Cbe7Library19active_snapshot_idsERNS_" &
-         "19Active_snapshot_idsE";
+         "_ZNK3Cbe7Library19active_snapshot_idsERNS_19Active_snapshot_idsE";
 
    function Max_VBA (Obj : Library.Object_Type)
    return Virtual_Block_Address_Type
